47ccaf2e8781f484ab72c4edbcb34cd1ab13116c
Vika Kazak <viktoria.kazak0@gmail.com>
2020-06-11 12:06:11 +0300

    homework from lesson 2.4

---
+++ L2.4.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"),'$100')
+    )
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+    button = browser.find_element_by_id("solve")
+    button.click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
